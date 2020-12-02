4cd6877e8514a63ca897097f1fdcb87d5169f4d4
O Ohio <tekzo4real@gmail.com>
2020-12-01 22:19:12 -0500

    Update main.tf
    Updated with provider template for each provider for resources will be deployed

--- main.tf
+++ main.tf
@@ -1,1 +1,38 @@
-#Root
+#################################
+##----root Multi Cloud module----
+#################################
+provider "aws" {
+  region = "${var.aws_region}"
+  alias  = "amazon"
+}
+
+provider "azurerm" {
+  version = "=1.43.0"
+  alias   = "microsoft"
+}
+
+provider "google" {
+  credentials = file("peak.json")
+  project     = "peak-hangout-267916"
+  region      = "us-east1"
+  alias       = "gcp"
+}
+
+# Deploy AWS Resources
+module "AWS" {
+  source       = "./storage"
+  project_name = "${var.project_name}"
+  provider     = "amazon"
+}
+
+# Deploy AZURE Resource
+module "AZURE" {
+  source       = "./AZURE"
+  provider     = "microsoft"
+}
+
+#Deploy GCP Resource
+module "GCP" {
+  source       = "./GCP"
+  provider     = "gcp"
+}
